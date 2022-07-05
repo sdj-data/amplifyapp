648c3ae82e86619d3dacd7076a24b6de08b0d445
Steven De Jesus <steven@coquihealth.com>
2022-07-04 22:34:05 -0400

    add graph ql 2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { API } from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listNotes } from './graphql/queries';
+import React, { useState, useEffect } from 'react'; 
+import './App.css'; 
+import { API } from 'aws-amplify'; 
+import { withAuthenticator } from '@aws-amplify/ui-react'; 
+import { listNotes } from './graphql/queries'; 
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
 const initialFormState = { name: '', description: '' }
 
-function App() {
-  const [notes, setNotes] = useState([]);
-  const [formData, setFormData] = useState(initialFormState);
+function App() {   const [notes, setNotes] = useState([]);   const [formData, setFormData] = useState(initialFormState);
 
   useEffect(() => {
-    fetchNotes();
-  }, []);
+    fetchNotes();   }, []);
 
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
-    setNotes(apiData.data.listNotes.items);
-  }
+    setNotes(apiData.data.listNotes.items);   }
 
   async function createNote() {
     if (!formData.name || !formData.description) return;
     await API.graphql({ query: createNoteMutation, variables: { input: formData } });
     setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
-  }
+    setFormData(initialFormState);   }
 
   async function deleteNote({ id }) {
     const newNotesArray = notes.filter(note => note.id !== id);
     setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
-  }
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});   }
 
   return (
     <div className="App">
@@ -58,9 +52,7 @@ function App() {
           ))
         }
       </div>
-      <AmplifySignOut />
-    </div>
-  );
-}
+      <withAuthenticator />
+    </div>   ); }
 
 export default withAuthenticator(App);
